b7149faa202e732ae2e0de8e9290978bdc5827b1
HarshPatel270698 <harshpatel270698@gmail.com>
2021-01-02 01:19:43 -0500

    op 1

--- Python/728. Self Dividing Numbers.py
+++ Python/728. Self Dividing Numbers.py
@@ -19,3 +19,25 @@ class Solution(object):
             else:
                 c=0
         return a
+		
+'''
+Runtime: 16 ms
+Memory Usage: 13.9 MB
+'''
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        a=[]
+        c=0
+        for i in range(left,right+1):
+            t=i
+            while t>0:
+                rem=t%10
+                if rem==0 or i%rem!=0:
+                    c+=1
+                    break
+                t//=10
+            if c==0:
+                a.append(i)
+            else:
+                c=0
+        return a
